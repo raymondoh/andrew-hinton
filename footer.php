ea69b8a828b0b28016a7d399650b52b503565770
raymondoh <raymondoh@users.noreply.github.com>
2025-07-04 13:30:30 +0100

    Initial commit

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * @package WP_Boilerplate
+ */
+?>
+
+</main><?php get_template_part('template-parts/components/site-footer'); ?>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
